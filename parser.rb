bb0e31928940ba0107ab40e1efbf85d3101e9e52
zzz0123 <zzz0123@test.com>
2012-10-03 00:38:36 +0900

    change output format array to json

--- parser.rb
+++ parser.rb
@@ -2,6 +2,7 @@
 class HtmlParser
   require 'nokogiri'
   require 'open-uri'
+  require 'json'
 
   DEFAULT_URL = "http://www.au.kddi.com/developer/android/kishu/device/index.html"
 
@@ -9,15 +10,28 @@ class HtmlParser
     @url = DEFAULT_URL
   end
 
+  def output
+    p data = parse
+    p convert_array_to_json(data)
+  end
+
+  def convert_array_to_json(data)
+    JSON.pretty_generate(data)
+  end
+
   def parse
     doc = Nokogiri::HTML(open(@url))
 
     table = doc.css("table")
-    table.css("tr").each do |elem|
-      p elem.content
+    array = []
+    table.css("tr").each do |tr|
+      elem = tr.content.split("\r\n")
+      model = elem.slice!(0)
+      array << { model => elem }
     end
+    array
   end
 end
 
 parser = HtmlParser.new
-parser.parse
+parser.output
